d6ad23de31e048e6e9f6a66651eec1fe090c7899
bll-shubham <Shubham.S@mindfiresolutions.com>
2024-05-23 17:00:22 +0530

    completed appointment management
    add
    search
    fetchall

--- routes/appointmentRoutes.js
+++ routes/appointmentRoutes.js
@@ -1,9 +1,18 @@
-const { addAppointmentController } = require("../controller/appointmentController");
+const {
+  addAppointmentController,
+  getAllAppointmentForToday,
+} = require("../controller/appointmentController");
 
 const express = require("express");
 const authMiddleware = require("../middleware/AuthMiddleware");
 
+// router object
+const router = express.Router();
 
 
 // ADD NEW APPOINTMENT
 router.post("/addNewAppointment", authMiddleware, addAppointmentController);
+
+router.get("/getAllAppointment", authMiddleware, getAllAppointmentForToday);
+
+module.exports = router;
